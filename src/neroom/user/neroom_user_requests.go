c0cf7f751b5d954bf7dbf3b4eab30b1154c78940
chengshuhua <chengshuhua@corp.netease.com>
2026-01-16 16:48:56 +0800

    请求格式定义错误，修改

--- src/neroom/user/neroom_user_requests.go
+++ src/neroom/user/neroom_user_requests.go
@@ -2,9 +2,9 @@ package user
 
 // CreateNeroomAccountRequest 创建Neroom账号请求
 type CreateNeroomAccountRequest struct {
-	UserUuid   string `json:"userUuid"`             // 用户UUID，全局唯一
-	Username   string `json:"username"`             // 用户名
-	Avatar     string `json:"avatar,omitempty"`     // 用户头像URL
-	Role       int    `json:"role,omitempty"`       // 用户角色，可选
-	ExpireTime int64  `json:"expireTime,omitempty"` // 账号过期时间戳，可选
+	UserUuid  string `json:"user_uuid"`
+	UserToken string `json:"user_token"`
+	UserName  string `json:"user_name"`
+	Icon      string `json:"icon"`
+	ImToken   string `json:"im_token"`
 }
